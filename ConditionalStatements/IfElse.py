ef26e43a071e0b288ef616c5aa16b5cea1c3a70b
Akshat Agrawal <akshatagrawal58@gmail.com>
2020-05-16 10:21:35 +0530

    Conditional Statements

---
+++ ConditionalStatements/IfElse.py
@@ -0,0 +1,7 @@
+x = 10
+if(x <= 5):
+    print("Smaller")
+elif (x <= 10):
+    print("Medium")
+else:
+    print("Larger")
---
+++ ConditionalStatements/Salary.py
@@ -0,0 +1,15 @@
+hrs = input("Enter Hours:")
+h = float(hrs)
+rate = input("Enter Rate: ")
+r = float(rate)
+
+if(h > 40):
+    reg = h * r
+    otp = (h - 40) * (r * 1.5)
+
+    pay = reg + otp
+
+else:
+    pay = h * r
+
+print(pay)
